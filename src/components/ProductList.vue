cc3dc6ddb823fbab5cc864dc1347e4e08c4d00c5
SimasDei <simondeiman@gmail.com>
2019-04-28 18:50:29 +0300

    use mock api data to loop through product list

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -2,17 +2,23 @@
   <div>
     <h1>Products List</h1>
     <ul>
-      <li v-for="product in products">{{product.title}} - {{product.price}}</li>
+      <li v-for="product in products" :key="product.id">{{product.title}} - {{product.price}}</li>
     </ul>
   </div>
 </template>
 
 <script>
+import shop from "../api/shop.js";
 export default {
   data() {
     return {
       products: []
     };
+  },
+  created() {
+    shop.getProducts(products => {
+      this.products = products;
+    });
   }
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import ProductList from '../components/ProductList.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'Product List',
+      component: ProductList
     }
   ]
 })
