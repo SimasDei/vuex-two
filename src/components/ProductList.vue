3766f4baf723cf51c4e45a140b9892891f06ea0b
SimasDei <simondeiman@gmail.com>
2019-04-28 18:41:29 +0300

    add product list component

---
+++ src/components/ProductList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Products List</h1>
+    <ul>
+      <li v-for="product in products">{{product.title}} - {{product.price}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: []
+    };
+  }
+};
+</script>
+
+<style>
+</style>
