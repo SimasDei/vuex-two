cc2d886922bee3f6fc920ee215e5bc8611e49fc5
Simas <simonas@digitouch.lt>
2019-04-30 14:58:15 +0300

    use the spread operator for mapState on computed method

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,11 +11,12 @@
 import { mapState } from "vuex";
 export default {
   name: "HelloWorld",
-  computed: mapState(["title"])
+  computed: {
+    ...mapState(["title"])
+  }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
