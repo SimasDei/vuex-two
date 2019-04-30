1d76d9caa69c4bc41b94d8cb5e4fb483c8572f12
Simas <simonas@digitouch.lt>
2019-04-30 15:52:53 +0300

    add remove all action

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -2,16 +2,31 @@
   <div class="stats">
     <h1>Stats Component</h1>
     <p>There are currently {{ countLinks }} links</p>
+    <button @click="removeAllLinks">Remove all Links</button>
+    <p>{{ msg }}</p>
   </div>
 </template>
-
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapMutations, mapActions } from "vuex";
 
 export default {
   name: "Stats",
+  data() {
+    return {
+      msg: ""
+    };
+  },
   computed: {
     ...mapGetters(["countLinks"])
+  },
+  methods: {
+    ...mapMutations(["REMOVE_ALL"]),
+    ...mapActions(["removeAll"]),
+    removeAllLinks() {
+      this.removeAll().then(() => {
+        this.msg = "Links have been removed o/";
+      });
+    }
   }
 };
 </script>
@@ -30,5 +45,14 @@ li {
 }
 a {
   color: #42b983;
+}
+button {
+  padding: 10px;
+  margin-top: 30px;
+  width: 100%;
+  background: none;
+  border: 1px solid #ccc;
+  outline: 0;
+  cursor: pointer;
 }
 </style>
